dc86fe9f8b76faded477dce2aa857b93e6d5b472
tom.finney@cd2solutions.co.uk <tom.finney@cd2solutions.co.uk>
2019-01-13 09:16:16 +0000

    starts teaser styles

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,53 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import PageWrapper from "../PageWrapper";
+
+import "./home.scss";
+
+const teasers = [
+  {
+    title: "A title about something",
+    summary: "Something something something something",
+    to: "/"
+  },
+  {
+    title: "A title about something",
+    summary: "Something something something something",
+    to: "/"
+  },
+  {
+    title: "A title about something",
+    summary: "Something something something something",
+    to: "/"
+  }
+];
 
 function Home() {
   return (
     <PageWrapper>
-      <div className="home">home!</div>
+      <div className="home">
+        <div>
+          <div className="contentTeaserPanel">
+            <h2>Blogs</h2>
+            <div className="contentTeasers">
+              {teasers.map(teaser => (
+                <div className="contentTeaser">
+                  <div>
+                    <div className="contentTeaser__img" />
+                    <div className="contentTeaser__content">
+                      <h3>{teaser.title}</h3>
+                      <p>{teaser.summary}</p>
+                      <Link className="btn" to={teaser.to}>
+                        Read More
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
     </PageWrapper>
   );
 }
