1e715a917075e743452936bd5920f1f6b421c175
tom.finney@cd2solutions.co.uk <tom.finney@cd2solutions.co.uk>
2019-01-27 16:36:04 +0000

    adds a mad mobile toggle

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink, Link } from "react-router-dom";
-import { MdHome, MdDescription, MdFace, MdWork } from "react-icons/md";
+import {
+  MdHome,
+  MdDescription,
+  MdFace,
+  MdWork,
+  MdMenu,
+  MdClose
+} from "react-icons/md";
 import routes from "../../constants/routes";
 
 import "./header.scss";
@@ -13,20 +20,30 @@ const links = [
 ];
 
 export default function Header() {
+  const [showNav, setShowNav] = useState(false);
   return (
     <header className="header">
       <div>
         <h2>
           <Link to={routes.home}>tomfinney.com</Link>
         </h2>
-        <nav className="navLinks">
-          {links.map(link => (
-            <NavLink key={link.to} to={link.to} exact={link.to === routes.home}>
-              <link.Icon />
-              <span>{link.text}</span>
-            </NavLink>
-          ))}
-        </nav>
+        <div className="headerInner">
+          <div className="navToggle" onClick={() => setShowNav(!showNav)}>
+            {showNav ? <MdClose /> : <MdMenu />}
+          </div>
+          <nav className={`navLinks ${showNav ? "navLinksShow" : ""}`}>
+            {links.map(link => (
+              <NavLink
+                key={link.to}
+                to={link.to}
+                exact={link.to === routes.home}
+              >
+                <link.Icon />
+                <span>{link.text}</span>
+              </NavLink>
+            ))}
+          </nav>
+        </div>
       </div>
     </header>
   );
