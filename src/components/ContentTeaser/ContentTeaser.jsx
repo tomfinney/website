720f6e924f82f7f8554e227f0453fcb13964f950
tom.finney@cd2solutions.co.uk <tom.finney@cd2solutions.co.uk>
2019-03-30 10:21:00 +0000

    better styling

--- src/components/ContentTeaser/ContentTeaser.jsx
+++ src/components/ContentTeaser/ContentTeaser.jsx
@@ -6,27 +6,27 @@ import cameraSvg from "../../assets/images/camera.svg";
 export default function ContentTeaser({ content, contentType }) {
   return (
     <div className="contentTeaser">
-      <div>
-        <div
-          className={`contentTeaser__imgContainer ${
-            content.meta.thumb ? "" : "translucent"
-          }`}
+      <div
+        className={`contentTeaser__imgContainer ${
+          content.meta.thumb ? "" : "translucent"
+        }`}
+      >
+        <img
+          src={content.meta.thumb ? content.meta.thumb : cameraSvg}
+          alt={content.meta.alt}
+        />
+      </div>
+      <div className="contentTeaser__content">
+        <h3>{content.meta.title}</h3>
+        <p>{content.meta.summary}</p>
+      </div>
+      <div className="contentTeaser__btnContainer">
+        <Link
+          className="btn"
+          to={`${routes[contentType]}${content.meta.handle}`}
         >
-          <img
-            src={content.meta.thumb ? content.meta.thumb : cameraSvg}
-            alt={content.meta.alt}
-          />
-        </div>
-        <div className="contentTeaser__content">
-          <h3>{content.meta.title}</h3>
-          <p>{content.meta.summary}</p>
-          <Link
-            className="btn"
-            to={`${routes[contentType]}${content.meta.handle}`}
-          >
-            Read More
-          </Link>
-        </div>
+          Read More
+        </Link>
       </div>
     </div>
   );
--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
 import App from "./components/App/App";
+import "unfetch/polyfill";
 
 ReactDOM.render(<App />, document.getElementById("root"));
