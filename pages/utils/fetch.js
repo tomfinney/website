05f8c21302dbad5815b671f9142746ebb8826f77
tom.finney@cd2solutions.co.uk <tom.finney@cd2solutions.co.uk>
2019-06-15 18:33:22 +0100

    cleaning

--- pages/utils/fetch.js
+++ pages/utils/fetch.js
@@ -1,8 +1,7 @@
 import iunfetch from "isomorphic-unfetch";
 
-const host = process.env.HOST ? process.env.HOST : "http://localhost";
-const port = process.env.PORT ? process.env.PORT : 3000;
+const host = process.env.HOST ? process.env.HOST : "http://localhost:3000";
 
 export function fetch(path) {
-  return iunfetch(`${host}:${port}${path}`);
+  return iunfetch(`${host}${path}`);
 }
