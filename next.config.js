c41ab2fa70c0e176b52feb54bb0a6f40d4a2d3ce
tom.finney@cd2solutions.co.uk <tom.finney@cd2solutions.co.uk>
2020-01-11 16:57:11 +0000

    updates next config to not be dumb with regards to setting env vars

--- next.config.js
+++ next.config.js
@@ -1,12 +1,8 @@
-const webpack = require("webpack");
-const { parsed: localEnv } = require("dotenv").config();
+require("dotenv").config();
 const withSass = require("@zeit/next-sass");
 
 module.exports = withSass({
-  webpack(config) {
-    config.plugins.push(new webpack.EnvironmentPlugin(localEnv));
-
-    return config;
-  },
-  target: "serverless"
+  env: {
+    HOST: process.env.HOST
+  }
 });
