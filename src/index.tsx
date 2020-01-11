3d485ac4f34ca7522c8192d99a18e4ee23db7fff
tom.finney@cd2solutions.co.uk <tom.finney@cd2solutions.co.uk>
2020-01-11 16:06:07 +0000

    removing legacy src folder and putting engines in package.json

--- src/index.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App/App";
-import "unfetch/polyfill";
-
-ReactDOM.render(<App />, document.getElementById("root"));
